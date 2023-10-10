703166babffd9dce1743d4f5e99d7e2093c15458
TDN <duynguyenqwert@gmail.com>
2023-10-10 16:38:04 +0700

    connect db atlas

--- config/db/index.js
+++ config/db/index.js
@@ -1,12 +1,28 @@
 const mongoose = require("mongoose");
 
+// async function connect() {
+//   try {
+//     await mongoose.connect("mongodb://localhost:27017/2hand_market", {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useCreateIndex: true,
+//     });
+//     console.log("Thành công!!!");
+//   } catch (error) {
+//     console.log("Thử lại!!!");
+//     console.log(error);
+//   }
+// }
 async function connect() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/2hand_market", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
+    await mongoose.connect(
+      "mongodb+srv://duynguyenqwert:duynguyenqwert@2handmarket.7kquoar.mongodb.net/2hand_market",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      }
+    );
     console.log("Thành công!!!");
   } catch (error) {
     console.log("Thử lại!!!");
