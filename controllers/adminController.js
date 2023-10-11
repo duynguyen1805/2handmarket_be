c1e2a17ed40e2230ee017d7c7de3d594b359a55b
TDN <duynguyenqwert@gmail.com>
2023-10-11 15:32:19 +0700

    fix cookie

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -83,7 +83,9 @@ class AdminController {
           });
 
           // res.cookie("jwt_token", `${token_1}`, { httpOnly: true });
-          res.cookie("jwt_token", `${token_1}`);
+          res.cookie("jwt_token", `${token_1}`, {
+            domain: `2handmarket.vercel.app`,
+          });
 
           return res.status(200).json({
             errCode: 0,
