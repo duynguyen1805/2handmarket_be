396803c8f3f9647be72124f2edcdbd4b39e07085
TDN <duynguyenqwert@gmail.com>
2023-10-12 07:27:43 +0700

    fix cookie 1.5.2

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -88,7 +88,7 @@ class AdminController {
           res.cookie("jwt_token", `${token_1}`, {
             // domain: allowedOrigins,
             sameSite: "None",
-            // secure: true,
+            secure: true,
           });
 
           return res.status(200).json({
