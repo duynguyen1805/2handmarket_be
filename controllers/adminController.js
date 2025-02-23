bae81e5cf6dd93a08552dfff4a40e9a467591587
TDN <duynguyenqwert@gmail.com>
2025-02-23 12:16:11 +0700

    feat: remove useAgent

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -57,8 +57,6 @@ class AdminController {
         output: videoFilePath,
         format: "bestvideo+bestaudio",
         mergeOutputFormat: "mp4", // Tự động ghép video + audio nếu có cài ffmpeg
-        userAgent:
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
         proxy:
           "http://brd-customer-hl_93c67cf2-zone-freemium:3x1s4b3e1v4c@brd.superproxy.io:33335",
       });
