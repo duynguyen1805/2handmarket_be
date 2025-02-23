44cd6c687d03a62e30a6d2479e95244bade6efe6
TDN <duynguyenqwert@gmail.com>
2025-02-23 12:33:52 +0700

    update: config

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -59,6 +59,11 @@ class AdminController {
         mergeOutputFormat: "mp4", // Tự động ghép video + audio nếu có cài ffmpeg
         proxy:
           "http://brd-customer-hl_93c67cf2-zone-freemium:3x1s4b3e1v4c@brd.superproxy.io:33335",
+        dumpSingleJson: true,
+        noCheckCertificates: true,
+        noWarnings: true,
+        preferFreeFormats: true,
+        addHeader: ["referer:youtube.com", "user-agent:googlebot"],
       });
 
       console.log(`✅ Video đã tải về: ${videoFilePath}`);
