1e2a6053b43589e584e672d66232e048b6375372
TDN <duynguyenqwert@gmail.com>
2025-02-24 15:58:50 +0700

    update: config

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -59,8 +59,9 @@ class AdminController {
         output: videoFilePath,
         format: "bestvideo+bestaudio",
         mergeOutputFormat: "mp4", // Tự động ghép video + audio nếu có cài ffmpeg
-        // proxy: "http://brd-customer-hl_93c67cf2-zone-freemium:3x1s4b3e1v4c@brd.superproxy.io:33335",
-        proxy: "http://alrfqysp:ta68euxebtn5@198.23.239.134:6540",
+        proxy:
+          "brd-customer-hl_93c67cf2-zone-freemium:3x1s4b3e1v4c@brd.superproxy.io:33335",
+        // proxy: "http://alrfqysp:ta68euxebtn5@198.23.239.134:6540",
         // dumpSingleJson: true,
         // noCheckCertificates: true,
         // noWarnings: true,
